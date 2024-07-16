907a9ce3a2e6c0adcf0537414c4b91d861128740
Lvl3Mage <lvlthreemage@gmail.com>
2024-07-16 17:56:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# Interpolation Package
+
+This package offers a collection of interpolation utilities for Unity projects.
+
+## Features
+- Frame-independent decay for smooth transitions between values.
+
+## Requirements
+- Unity 2021.3 or later
+
+## Installation
+To use the Interpolation package in your project, import the `com.Lvl3Mage.interpolation` package.
+
+## Usage
+The `Decay` class provides static methods to smoothly interpolate between two values, angles, or vectors over time, independent of the frame rate. This is particularly useful for animations and movement smoothing.
+
+Example usage:
+```csharp
+float newValue = Decay.To(currentValue, targetValue, speed, Time.deltaTime);
---
+++ Runtime/Decay.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace Interpolation
+{
+	public static class Decay
+    {
+    	/// <summary>
+    	/// Framerate independent decay between 2 values
+    	/// </summary>
+    	/// <param name="from">Start value</param>
+    	/// <param name="to">Target value</param>
+    	/// <param name="speed">Speed of the decay, usually a value between 1 and 25</param>
+    	/// <param name="deltaTime">Delta time since the last update</param>
+    	/// <returns></returns>
+    	public static float To(float from, float to, float speed, float deltaTime)
+    	{
+    		return to+(from-to)*Mathf.Exp(-speed*deltaTime);
+    	}
+	    /// <summary>
+    	/// Framerate independent decay between 2 values
+    	/// </summary>
+    	/// <param name="from">Start value</param>
+    	/// <param name="to">Target value</param>
+    	/// <param name="speed">Speed of the decay, usually a value between 1 and 25</param>
+    	/// <param name="deltaTime">Delta time since the last update</param>
+    	/// <returns></returns>
+    	public static float ToZoom(float from, float to, float speed, float deltaTime)
+    	{
+		    float logA = Mathf.Log(from);
+		    float logB = Mathf.Log(to);
+    		float result = logB+(logA-logB)*Mathf.Exp(-speed*deltaTime);
+		    return Mathf.Exp(result);
+    	}
+    	/// <summary>
+    	/// Framerate independent decay between 2 angles
+    	/// </summary>
+    	/// <param name="from">Start angle</param>
+    	/// <param name="to">Target angle</param>
+    	/// <param name="speed">Speed of the decay, usually a value between 1 and 25</param>
+    	/// <param name="deltaTime">Delta time since the last update</param>
+    	/// <returns></returns>
+    	public static float ToAngle(float from, float to, float speed, float deltaTime)
+    	{
+    		float delta = Mathf.DeltaAngle(from,to);
+    		float target = from + delta;
+    		return To(from, target, speed, deltaTime);
+    
+    	}
+    	/// <summary>
+    	/// Framerate independent decay between 2 vectors
+    	/// </summary>
+    	/// <param name="from">Start vector</param>
+    	/// <param name="to">Target vectos</param>
+    	/// <param name="speed">Speed of the decay, usually a value between 1 and 25</param>
+    	/// <param name="deltaTime">Delta time since the last update</param>
+    	/// <returns></returns>
+    	public static Vector2 To(Vector2 from, Vector2 to, float speed, float deltaTime)
+    	{
+    		return to+(from-to)*Mathf.Exp(-speed*deltaTime);
+    	}
+    	/// <summary>
+    	/// Framerate independent decay between 2 vectors
+    	/// </summary>
+    	/// <param name="from">Start vector</param>
+    	/// <param name="to">Target vector</param>
+    	/// <param name="speed">Speed of the decay</param>
+    	/// <param name="deltaTime">Delta time since the last update</param>
+    	/// <returns></returns>
+    	public static Vector3 To(Vector3 from, Vector3 to, float speed, float deltaTime)
+    	{
+    		return to+(from-to)*Mathf.Exp(-speed*deltaTime);
+    	}
+	    
+    }
+}
